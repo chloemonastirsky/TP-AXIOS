c754c6fa0cdd170fe1320f7ad3dec9040af198b8
CHLOE MONASTIRSKY <49552251@ort>
2026-05-26 08:15:35 -0300

    errror messager + Loader

---
+++ @latest/src/assets/Componentes/ErrorMessage.jsx
@@ -0,0 +1,22 @@
+ import axios from "axios";
+import { useState, useEffect } from 'react';
+
+
+const ErrorMessage = ({}) => {
+    
+    return (
+
+        <div>
+
+            {
+                    <div >
+                        <h2>Error</h2>
+                    </div>
+     
+            }
+
+        </div>
+    );
+};
+
+export default ErrorMessage;
---
+++ @latest/src/assets/Componentes/Loader.jsx
@@ -0,0 +1,21 @@
+ import axios from "axios";
+import { useState, useEffect } from 'react';
+
+const Loader = ({}) => {
+
+    return (
+
+        <div>
+
+            {
+                    <div >
+                        <h2>Loading...</h2>
+                    </div>
+     
+            }
+
+        </div>
+    );
+};
+
+export default Loader;
